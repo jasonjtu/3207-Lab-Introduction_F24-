a4f8d5b6940478e4aa6c7c6925c6eb1029970052
jasonjtu <tul32326@temple.edu>
2025-01-19 18:07:48 -0500

    Update random.c

--- random.c
+++ random.c
@@ -5,3 +5,9 @@ char randchar() {
     srand(time(NULL)); // Seed the random number generator
     return 'A' + (rand() % 26); // Generate a random character between 'A' and 'Z'
 }
+
+int main() {
+    char randomChar = randchar();
+    printf("Random Character: %c\n", randomChar);
+    return 0;
+}
